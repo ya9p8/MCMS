b32004eb6e75c5768483376503f7604536bc6b21
Yemi Ajibola <ya9p8@mst.edu>
2016-01-19 13:39:23 -0600

    Initial Commit

---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Yemi Ajibola on 1/19/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
